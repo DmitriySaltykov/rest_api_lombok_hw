69424489030130fb579e868fda2aefb2a70b85aa
DSaltykov <spitfiredia@gmail.com>
2023-10-05 11:39:17 +0300

    added login test with lombok

--- src/test/java/api_lombok_test/test/LoginTest.java
+++ src/test/java/api_lombok_test/test/LoginTest.java
@@ -4,108 +4,66 @@ package api_lombok_test.test;
 import api_lombok_test.models.LoginBodyModel;
 import api_lombok_test.models.LoginResponseModel;
 import api_lombok_test.models.LoginErrorModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.LoginSpec.*;
 
 
 public class LoginTest extends TestBase {
     @Test
+    @DisplayName("Successful login test")
     void successfulLoginTest() {
 
         LoginBodyModel authData = new LoginBodyModel();
         authData.setEmail("eve.holt@reqres.in");
         authData.setPassword("cityslicka");
 
-        LoginResponseModel response = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(JSON)
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().as(LoginResponseModel.class);
+        LoginResponseModel response = step("Login user", () ->
+                given(loginRequestSpec)
+                        .body(authData)
+                        .when()
+                        .post("/login")
+                        .then()
+                        .spec(loginResponseSpec)
+                        .extract().as(LoginResponseModel.class));
 
-        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+        step("Checking the answer", () ->
+                assertEquals("QpwL5tke4Pnpja7X4", response.getToken())
+        );
 
 
     }
 
 
     @Test
+    @DisplayName("Unsuccessful login test")
     void unsuccessfulTest() {
 
         LoginBodyModel authData = new LoginBodyModel();
-        authData.setEmail("eve.holt@reqres.in");
+
         authData.setPassword("cityslicka");
 
-        LoginErrorModel response = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .extract().as(LoginErrorModel.class);
-        assertEquals("Missing email or username", response.getError());
+        LoginErrorModel response = step("Login user", () ->
+                given(loginRequestSpec)
+                        .body(authData)
+                        .when()
+                        .post("/login")
+                        .then()
+                        .spec(loginResponseErrorSpec)
+                        .extract().as(LoginErrorModel.class));
+
+        step("Checking the answer", () ->
+                assertEquals("Missing email or username", response.getError())
+        );
+
 
     }
 
 
-
-    @Test
-    void emptyPasswordTest() {
-        LoginBodyModel authData = new LoginBodyModel();
-        authData.setEmail("eve.holt@reqres.in");
-        authData.setPassword("");
-
-        LoginErrorModel response = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(JSON)
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .extract().as(LoginErrorModel.class);
-        assertEquals("Missing password", response.getError());
-    }
-
-    @Test
-    void emptyEmailTest() {
-        LoginBodyModel authData = new LoginBodyModel();
-        authData.setEmail("");
-        authData.setPassword("cityslicka");
-
-        LoginErrorModel response = given()
-                .log().uri()
-                .log().method()
-                .log().body()
-                .contentType(JSON)
-                .body(authData)
-                .when()
-                .post("/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .extract().as(LoginErrorModel.class);
-        assertEquals("Missing email or username", response.getError());
-    }
 }
