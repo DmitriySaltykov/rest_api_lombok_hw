0a39fababc7230b4f2e04fec6f62fa9cdb3c43b8
DSaltykov <spitfiredia@gmail.com>
2023-11-09 14:34:50 +0300

    added readme file

--- src/test/java/api_lombok_test/test/TestBase.java
+++ src/test/java/api_lombok_test/test/TestBase.java
@@ -1,9 +1,9 @@
 package api_lombok_test.test;
 
 import config.ApiConfig;
-import io.restassured.RestAssured;
+
 import org.aeonbits.owner.ConfigFactory;
-import org.junit.jupiter.api.BeforeAll;
+
 
 public class TestBase {
     protected static final ApiConfig config = ConfigFactory.create(ApiConfig.class, System.getProperties());
